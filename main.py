b8c85ebc43b67ff81a25cbe0ba85f7c5881c0e71
Frikk Brændsrød <fribra05@yahoo.com>
2025-01-11 20:22:36 +0100

    Made main.py automaticly push and pull the file to GitHub

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 #Python file
 #Author: Frikk Brændsrød
+#Date: 2021-09-26
+#Version: 1.0
 
-import os
 
-
-import os
 import shutil
+import subprocess
 
-print("Remeber to pull the repo for the latest save file")
+print("")
+print("")
 input = int(input("update to new save = 1, backup current save = 2: "))
+print("You chose: ", input)
+print("")
+print("------------")
 
 
 source_path = r'current_save.zip'
 destination_path = r'C:\Users\fribr\AppData\Roaming\Factorio\saves\Gjøvik.zip'
 
 if input == 1:
-    shutil.copyfile(destination_path, source_path)
+    shutil.copyfile(source_path, destination_path)
+    subprocess.run(["git", "pull"])
+    print("Updated to new save")
 
 elif input == 2:
-    shutil.copyfile(source_path, destination_path)
+    shutil.copyfile(destination_path, source_path)
+
+    subprocess.run(["git", "add", destination_path])
+    subprocess.run(["git", "commit", "-m", "Updated latest save file"])
+    subprocess.run(["git", "push"])
+    print("Backup of current save done")
+
 
 else:
     print("Invalid input")
